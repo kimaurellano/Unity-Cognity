4addfc90b859a67ea1bf8c16bb2efeb81611054c
kimaurellano <altaiirdesmond@gmail.com>
2019-06-01 12:22:55 +0800

    Update code refactor

--- src/Assets/Scripts/Memory/Card.cs
+++ src/Assets/Scripts/Memory/Card.cs
@@ -1,25 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-namespace Assets.Scripts.Memory
-{
-    public class Card : MonoBehaviour
-    {
+namespace Assets.Scripts.Memory {
+    public class Card : MonoBehaviour {
         [SerializeField] private bool _locked;
 
-        public bool Locked { get => _locked; set => _locked = value; }
-
-        private void Start()
-        {
-            //StartCoroutine(FaceUp());
-        }
-
-        private IEnumerator FaceUp()
-        {
-            yield return new WaitForSeconds(5f);
-
-            GetComponent<Animator>().SetBool("flip", true);
+        public bool Locked {
+            get => _locked;
+            set => _locked = value;
         }
     }
 }
