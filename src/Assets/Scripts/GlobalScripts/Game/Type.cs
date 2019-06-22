e3c320e77faa366ff9a9cb6e4469d1f3f6e19510
kimaurellano <kim.aurellano@yahoo.com>
2019-06-22 18:27:11 +0800

    Refactored GameType namespace

---
+++ src/Assets/Scripts/GlobalScripts/Game/Type.cs
@@ -0,0 +1,10 @@
+﻿namespace Assets.Scripts.GlobalScripts.Game {
+    public static class Type {
+        public enum GameType {
+            Flexibility = 1,
+            Memory = 2,
+            ProblemSolving = 4,
+            Language = 3
+        }
+    }
+}
