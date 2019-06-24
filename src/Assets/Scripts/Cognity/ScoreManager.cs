f7f966a47f95cd72e6226b8adea2eabbd949cb23
kimaurellano <kim.aurellano@yahoo.com>
2019-06-24 11:16:36 +0800

    Code refactor

--- src/Assets/Scripts/Cognity/ScoreManager.cs
+++ src/Assets/Scripts/Cognity/ScoreManager.cs
@@ -1,6 +1,4 @@
-﻿using UnityEngine;
-
-namespace Assets.Scripts.Cognity {
+﻿namespace Assets.Scripts.Cognity {
     public class ScoreManager {
         public float TotalScore { get; private set; }
 
@@ -13,19 +11,6 @@ namespace Assets.Scripts.Cognity {
 
             // Add score. We collect score as seconds
             TotalScore += minToSec + seconds;
-        }
-
-        /// <summary>
-        ///     Display score as Min:Sec
-        /// </summary>
-        /// <returns>Converted seconds to a corresponding minute</returns>
-        public string GetTotalScore() {
-            float seconds = TotalScore / 60f;
-
-            seconds = Mathf.Repeat(seconds, 1.0f) * 60f;
-            int minutes = (int) TotalScore / 60;
-
-            return $"{minutes:00}:{seconds:00}";
         }
     }
 }
