b14b4105d055569a2888c4d686ca8562d90c571a
kimaurellano <kim.aurellano@yahoo.com>
2019-10-08 18:06:51 +0800

    Updated StatsManager.cs

--- src/Assets/Scripts/GlobalScripts/UIComponents/StatsManager.cs
+++ src/Assets/Scripts/GlobalScripts/UIComponents/StatsManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using Assets.Scripts.GlobalScripts.UITask;
+using TMPro;
 using UnityEngine;
 
 namespace Assets.Scripts.GlobalScripts.UIComponents {
@@ -10,11 +11,12 @@ namespace Assets.Scripts.GlobalScripts.UIComponents {
 
         [SerializeField] private GameObject _radarMesh;
 
+        private UIManager _uiManager;
+
         private const float MAX_VALUE = 35;
-        private float _radarRadius;
 
         private void Awake() {
-            _radarRadius = _radarMesh.GetComponent<RectTransform>().sizeDelta.x;
+            _uiManager = FindObjectOfType<UIManager>();
         }
 
         /// <summary>
@@ -67,18 +69,26 @@ namespace Assets.Scripts.GlobalScripts.UIComponents {
             float defaultHeight = 1;
 
             Vector3[] vertices = new Vector3[4];
-            
-            float problemSolvingPercent = PlayerPrefs.GetFloat("ProblemSolving") * MAX_VALUE;
-            vertices[0] = new Vector3(-defaultWidth - problemSolvingPercent, -defaultHeight - problemSolvingPercent);
 
-            float memoryPercent = PlayerPrefs.GetFloat("Memory") * MAX_VALUE;
-            vertices[1] = new Vector3(-defaultWidth - memoryPercent, defaultHeight + memoryPercent);
+            float problemSolvingProgress = PlayerPrefs.GetFloat("ProblemSolving");
+            TextMeshProUGUI problemSolvingPercentText = (TextMeshProUGUI) _uiManager.GetUI(UIManager.UIType.Text, "problem solving");
+            problemSolvingPercentText.SetText((problemSolvingProgress * 100) + "%");
+            vertices[0] = new Vector3(-defaultWidth - problemSolvingProgress * MAX_VALUE, -defaultHeight - problemSolvingProgress * MAX_VALUE);
 
-            float flexibilityPercent = PlayerPrefs.GetFloat("Flexibility") * MAX_VALUE;
-            vertices[2] = new Vector3(defaultWidth + flexibilityPercent, defaultHeight + flexibilityPercent);
+            float memoryProgress = PlayerPrefs.GetFloat("Memory");
+            TextMeshProUGUI memoryPercentText = (TextMeshProUGUI)_uiManager.GetUI(UIManager.UIType.Text, "memory");
+            memoryPercentText.SetText((memoryProgress * 100) + "%");
+            vertices[1] = new Vector3(-defaultWidth - memoryProgress * MAX_VALUE, defaultHeight + memoryProgress * MAX_VALUE);
 
-            float languagePercent = PlayerPrefs.GetFloat("Language") * MAX_VALUE;
-            vertices[3] = new Vector3(defaultWidth + languagePercent, -defaultHeight - languagePercent);
+            float flexibilityProgress = PlayerPrefs.GetFloat("Flexibility");
+            TextMeshProUGUI flexibilityPercentText = (TextMeshProUGUI)_uiManager.GetUI(UIManager.UIType.Text, "flexibility");
+            flexibilityPercentText.SetText((flexibilityProgress * 100) + "%");
+            vertices[2] = new Vector3(defaultWidth + flexibilityProgress * MAX_VALUE, defaultHeight + flexibilityProgress * MAX_VALUE);
+
+            float languageProgress = PlayerPrefs.GetFloat("Language");
+            TextMeshProUGUI languagePercentText = (TextMeshProUGUI)_uiManager.GetUI(UIManager.UIType.Text, "language");
+            languagePercentText.SetText((languageProgress * 100) + "%");
+            vertices[3] = new Vector3(defaultWidth + languageProgress * MAX_VALUE, -defaultHeight - languageProgress * MAX_VALUE);
 
             int[] triangles = new int[] { 0, 1, 2, 0, 2, 3 };
 
