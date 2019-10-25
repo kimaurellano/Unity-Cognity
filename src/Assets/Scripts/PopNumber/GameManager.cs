1734020e806b995db57d074dfdf15eb1139f94e4
kimaurellano <kim.aurellano@yahoo.com>
2019-10-25 09:59:29 +0800

    Added PopNumber game

---
+++ src/Assets/Scripts/PopNumber/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using Assets.Scripts.GlobalScripts.Managers;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.PopNumber {
+    public class GameManager : CoreGameBehaviour {
+
+        [SerializeField] private GameObject _numberPrefab;
+        [SerializeField] private float _spawnPositionMinX;
+        [SerializeField] private float _spawnPositionMaxX;
+        [SerializeField] private float _spawnPositionY;
+
+        private TimerManager _timerManager;
+        private float _quarter = 45f;
+        private float _speedIncrease = 1f;
+        private float _spawnTime = 1.5f;
+        private float _wait;
+
+        private void Start() {
+            _timerManager = GetComponent<TimerManager>();
+            _timerManager.StartTimerAt(1, 0f);
+
+            StartCoroutine(StartSpawning());
+        }
+
+        private void Update() {
+            if (_timerManager.Seconds == _quarter) {
+                _wait = 5f;
+                _spawnTime -= 0.3f;
+                _speedIncrease++;
+
+                _quarter = _timerManager.Seconds - 15f;
+                Debug.Log(_quarter);
+            }
+        }
+
+        private IEnumerator StartSpawning() {
+            while (!_timerManager.TimerUp) {
+                GameObject spawnedPrefab = Instantiate(
+                    _numberPrefab,
+                    new Vector3(Random.Range(_spawnPositionMinX, _spawnPositionMaxX), _spawnPositionY, 0f),
+                    Quaternion.identity);
+
+                spawnedPrefab.GetComponent<NumberScript>().MoveSpeed = _speedIncrease;
+
+                yield return new WaitForSeconds(_spawnTime);
+
+                while (_wait > -1) {
+                    yield return new WaitForSeconds(1f);
+                    _wait--;
+                    Debug.Log("Wait time:" + _wait);
+                }
+            }
+
+            yield break;
+        }
+    }
+}
---
+++ src/Assets/Scripts/PopNumber/NumberScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.PopNumber {
+    public class NumberScript : MonoBehaviour {
+
+        public float MoveSpeed { get; set; }
+
+        private void Update() {
+            transform.Translate(Vector3.down * Time.deltaTime * MoveSpeed);
+        }
+
+        private void OnBecameInvisible() {
+            Destroy(gameObject);
+        }
+    }
+}
