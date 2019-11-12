1b57053ae9b846c1ced458ed490162730f2ea790
kimaurellano <kim.aurellano@yahoo.com>
2019-11-12 12:45:36 +0800

    Added WordMemory game

---
+++ src/Assets/Scripts/WordMemory/GameManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using Assets.Scripts.GlobalScripts.Game;
+using Assets.Scripts.GlobalScripts.Managers;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
+
+namespace Assets.Scripts.WordMemory {
+    public class GameManager : CoreGameBehaviour {
+        [SerializeField] private string[] _listOfWords;
+        [SerializeField] private TextMeshProUGUI _word;
+
+        private TimerManager _timerManager;
+        private UIManager _uiManager;
+        private string _temp;
+        private string _str;
+
+        private void Start() {
+            _timerManager = GetComponent<TimerManager>();
+            _uiManager = FindObjectOfType<UIManager>();
+
+            TimerManager.OnPreGameTimerEndEvent += StartGame;
+        }
+
+        private void StartGame() {
+            TimerManager.OnPreGameTimerEndEvent -= StartGame;
+
+            _timerManager.StartTimerAt(0, 10f);
+
+            DisplayWord();
+        }
+
+        private void DisplayWord() {
+            _str = _listOfWords[Random.Range(0, _listOfWords.Length - 1)];
+            _word.SetText(_str);
+
+            Debug.Log($"<color=orange>Current word:{_str}</color>");
+        }
+
+        public override void EndGame() {
+            // Clear
+            TimerManager.OnGameTimerEndEvent -= DisplayWord;
+
+            // TODO: implement scoring
+
+            base.EndGame();
+
+            SceneManager.LoadScene(GetNextScene());
+        }
+
+        public void Button(string userAnswer) {
+            string answer = _str == _temp ? "true" : "false";
+
+            if (userAnswer.Equals(answer)) {
+                _timerManager.StartTimerAt(0, 10f);
+
+                TextMeshProUGUI textUI = (TextMeshProUGUI)_uiManager.GetUI(UIManager.UIType.Text, "score change");
+                textUI.color = Color.green;
+                textUI.text = "correct!";
+            } else {
+                TextMeshProUGUI textUI = (TextMeshProUGUI)_uiManager.GetUI(UIManager.UIType.Text, "score change");
+                textUI.color = Color.red;
+                textUI.text = "wrong";
+            }
+
+            Animation anim = (Animation)_uiManager.GetUI(UIManager.UIType.AnimatedSingleState, "score change");
+            anim.Play();
+
+            // Remember the last displayed word
+            _temp = _str;
+            Debug.Log($"Last word is now:{_temp}");
+
+            StartCoroutine(ChangeWord());
+        }
+
+        private IEnumerator ChangeWord() {
+            _word.SetText(string.Empty);
+            yield return new WaitForSeconds(0.5f);
+            DisplayWord();
+        }
+    }
+}
